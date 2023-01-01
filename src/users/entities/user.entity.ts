de8368a6e1d28294edfdfa45a4a275335588bb83
S A Saharukh <sasaharukh@gmail.com>
2023-01-02 00:09:14 +0600

    emailVerified added

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -18,6 +18,12 @@ export class User {
   @Prop({ required: true })
   password: string;
 
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, default: false })
+  emailVerified: boolean;
+
   @Prop()
   refreshToken: string;
 }
